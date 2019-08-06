743108559fdb13ae8ee0daea1c4f7b5a1b24e5b2
Jun Liang <illuminatemynight@gmail.com>
2019-08-06 03:30:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,10 @@
 require 'pry'
 def consolidate_cart(cart)
   hash_cart = {}
-  cart.each do |item| 
+  cart.each do |item|
+    if hash_cart.include?(item.keys[0])
+      
+      hash_cart[item.keys[0]][:count] += 1
   binding.pry
   end
 end
